449d06633498b01d212bfe3e3a9d836d7d6d3c85
Mitchell Frazer <108438939+moosieth@users.noreply.github.com>
2023-04-02 22:56:31 -0700

    feat!: create basic flask app
    Create base Flask app to handle HTTP requests to DB
    
    BREAKING CHANGE: Not fully implemented yet

---
+++ http_service/http_service.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import json
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET'])
+def login():
+    uid = request.args.get('uid')
+    password = request.args.get('password')
+
+    print(uid)
+    print(password)
+
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    cursor.execute(f'SELECT userID, pw FROM User WHERE userID={uid} AND pw="{password}"')
+    tuples = cursor.fetchall()
+
+    return jsonify(tuples)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
