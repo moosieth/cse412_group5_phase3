e9bffbb6a2b23c040196cb2bd7492841454aec3c
Mitchell Frazer <108438939+moosieth@users.noreply.github.com>
2023-04-03 20:28:52 -0700

    feat: implement login and register paths
    Implement register and login paths for Flask API
    
    BREAKING CHANGE: Not complete implementation. Needs more functionalities

--- http_service/http_service.py
+++ http_service/http_service.py
@@ -1,12 +1,35 @@
 from flask import Flask, request, jsonify
+import hashlib
 import json
 import mysql.connector
 
 app = Flask(__name__)
 
+@app.route('/register', methods=['POST'])
+def register():
+    newUser = request.json
+
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    query = f'INSERT INTO User VALUES({newUser["userID"]}, "{newUser["fName"]}", "{newUser["lName"]}", "{newUser["town"]}", "{newUser["gender"]}", "{newUser["pw"]}", "{newUser["email"]}", "{newUser["dob"]}")'
+    print(query)
+
+    try:
+        cursor.execute(query)
+        con.commit()
+        cursor.close()
+        con.close()
+
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    except mysql.connector.Error as e:
+        print("MYSQL EXECUTION ERROR: {}".format(e))
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+        
+
 @app.route('/login', methods=['GET'])
 def login():
-    uid = request.args.get('uid')
+    uid = request.args.get('email')
     password = request.args.get('password')
 
     print(uid)
@@ -15,10 +38,13 @@ def login():
     con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
     cursor = con.cursor()
 
-    cursor.execute(f'SELECT userID, pw FROM User WHERE userID={uid} AND pw="{password}"')
+    cursor.execute(f'SELECT userID, pw FROM User WHERE email="{uid}" AND pw="{password}"')
     tuples = cursor.fetchall()
 
-    return jsonify(tuples)
+    if(tuples):
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    else:
+        return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
     app.run()
