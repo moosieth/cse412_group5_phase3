bc0056bb37146ee842dbd9cf0ca24423248f0be2
Mitchell Frazer <108438939+moosieth@users.noreply.github.com>
2023-04-04 11:14:23 -0700

    build: change database id values to auto increments
    Change ID values for User, Album, Photo, and Comment to auto increments on DB startup

--- db_image/schema.sql
+++ db_image/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS db;
 
 CREATE TABLE IF NOT EXISTS db.User(
-	userID INTEGER NOT NULL,
+	userID INTEGER NOT NULL AUTO_INCREMENT,
 	fName VARCHAR(30),
 	lName VARCHAR(30),
 	town VARCHAR(30),
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS db.User(
 );
 
 CREATE TABLE IF NOT EXISTS db.Album(
-	albumID INTEGER, 
+	albumID INTEGER NOT NULL AUTO_INCREMENT, 
 	userID INTEGER,
 	name VARCHAR(30),
 	dateCreated DATE,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS db.Friends(
 );
 
 CREATE TABLE IF NOT EXISTS db.Photo(
-	photoID INTEGER, 
+	photoID INTEGER NOT NULL AUTO_INCREMENT, 
 	albumID INTEGER NOT NULL,
 	caption VARCHAR(50),
 	data VARCHAR(50),
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS db.Tag(
 
 
 CREATE TABLE IF NOT EXISTS db.Comment(
-	commentID INTEGER,
+	commentID INTEGER NOT NULL AUTO_INCREMENT,
 	content VARCHAR(120),
 	dateCreated DATE,
 	userID INTEGER,
@@ -68,51 +68,63 @@ CREATE TABLE IF NOT EXISTS db.Comment(
 	FOREIGN KEY (photoID) REFERENCES db.Photo(photoID) ON DELETE CASCADE
 );
 
-INSERT INTO db.User VALUES (100001,'Stephen','Strange','Manhattan','M','1ab2cd','s.strange@kamertaj.org','1987-02-15 00:00:00'),
-	(100002,'Tony','Stark','Miami','M','3ef4gh','stark@stark.com','1988-05-27 00:00:00'),
-	(100003,'Steve','Rogers','Brooklyn','M','5ij6kl','s.rogers@army.mil','1925-03-13 00:00:00'),
-	(100004,'Bruce','Banner','Dayton','M','7nm8op','b.bbanner@shield.gov','1979-07-17 00:00:00'),
-	(100005,'Thor','Odinson','Asgard','M','9qr0st','th0r_r0ckz@gmail.com','1990-09-30 00:00:00'),
-	(100006,'Peter','Parker','Queens','M','1uv2wx','nycwebcrawler@gmail.com','2001-01-15 00:00:00'),
-	(100007,'Natasha','Romanoff','Moscow','F','3yz4ab','n.romano@shield.gov','1992-11-22 00:00:00'),
-	(100008,'Scott','Lang','Coral Gables','M','5cd6ef','antman_avenger@gmail.com','1989-10-10 00:00:00'),
-	(100009,'Steven','Grant','London','M','7gh8ij','steveng123@gmail.com','1992-09-19 00:00:00'),
-	(100010,'Marc','Spector','Cairo','M','9kl0mn','notmarcspector@gmail.com','1992-09-19 00:00:00');
+ALTER TABLE db.User AUTO_INCREMENT=100000;
 
-INSERT INTO db.Album VALUES (200001,100009,'"Art"','2023-01-30 00:00:00'),
-	(200002,100002,'"Suits"','2023-01-30 00:00:00'),
-	(200003,100003,'"Fitness"','2023-01-31 00:00:00'),
-	(200004,100009,'"Food"','2023-01-31 00:00:00'),
-	(200005,100005,'"Midgard"','2023-02-01 00:00:00'),
-	(200006,100002,'"Travel"','2023-02-02 00:00:00'),
-	(200007,100002,'"Cars"','2023-02-02 00:00:00'),
-	(200008,100005,'"Asgard"','2023-02-02 00:00:00'),
-	(200009,100006,'"Retro Tech"','2023-02-05 00:00:00'),
-	(200010,100004,'"Science"','2023-02-05 00:00:00');
+ALTER TABLE db.Album AUTO_INCREMENT=200000;
 
-INSERT INTO db.Friends VALUES (100001.0,100003.0,'2023-01-30 00:00:00'),
-	(100001.0,100010.0,'2023-01-31 00:00:00'),
-	(100003.0,100001.0,'2023-01-31 00:00:00'),
-	(100005.0,100003.0,'2023-02-01 00:00:00'),
-	(100007.0,100003.0,'2023-02-01 00:00:00'),
-	(100007.0,100005.0,'2023-02-03 00:00:00'),
-	(100008.0,100007.0,'2023-02-07 00:00:00'),
-	(100009.0,100002.0,'2023-02-09 00:00:00'),
-	(100002.0,100009.0,'2023-02-09 00:00:00');
+ALTER TABLE db.Photo AUTO_INCREMENT=300000;
 
-INSERT INTO db.Photo VALUES (300001,200001,'This is a caption','/path/to/photo1.jpg'),
-	(300002,200002,'This is another caption','/path/to/photo2.jpg'),
-	(300003,200003,'We''ve got so many captions','/path/to/photo3.jpg'),
-	(300004,200002,'Yet another caption','/path/to/photo4.jpg'),
-	(300005,200004,'Something about captions','/path/to/photo5.jpg'),
-	(300006,200005,'Captions?','/path/to/photo6.jpg'),
-	(300007,200004,'Captions!','/path/to/photo7.jpg'),
-	(300008,200005,'Wowie, another caption!','/path/to/photo8.jpg'),
-	(300009,200006,'Will the captions ever stop?','/path/to/photo9.jpg'),
-	(300010,200007,'Yep. This is the last caption.','/path/to/photo10.jpg');
+ALTER TABLE db.Comment AUTO_INCREMENT=400000;
+
+INSERT INTO db.User (fName, lName, town, gender, pw, email, dob) VALUES 
+	('Stephen','Strange','Manhattan','M','1ab2cd','s.strange@kamertaj.org','1987-02-15 00:00:00'),
+	('Tony','Stark','Miami','M','3ef4gh','stark@stark.com','1988-05-27 00:00:00'),
+	('Steve','Rogers','Brooklyn','M','5ij6kl','s.rogers@army.mil','1925-03-13 00:00:00'),
+	('Bruce','Banner','Dayton','M','7nm8op','b.bbanner@shield.gov','1979-07-17 00:00:00'),
+	('Thor','Odinson','Asgard','M','9qr0st','th0r_r0ckz@gmail.com','1990-09-30 00:00:00'),
+	('Peter','Parker','Queens','M','1uv2wx','nycwebcrawler@gmail.com','2001-01-15 00:00:00'),
+	('Natasha','Romanoff','Moscow','F','3yz4ab','n.romano@shield.gov','1992-11-22 00:00:00'),
+	('Scott','Lang','Coral Gables','M','5cd6ef','antman_avenger@gmail.com','1989-10-10 00:00:00'),
+	('Steven','Grant','London','M','7gh8ij','steveng123@gmail.com','1992-09-19 00:00:00'),
+	('Marc','Spector','Cairo','M','9kl0mn','notmarcspector@gmail.com','1992-09-19 00:00:00');
+
+INSERT INTO db.Album (userID, name, dateCreated) VALUES 
+	(100009,'"Art"','2023-01-30 00:00:00'),
+	(100002,'"Suits"','2023-01-30 00:00:00'),
+	(100003,'"Fitness"','2023-01-31 00:00:00'),
+	(100009,'"Food"','2023-01-31 00:00:00'),
+	(100005,'"Midgard"','2023-02-01 00:00:00'),
+	(100002,'"Travel"','2023-02-02 00:00:00'),
+	(100002,'"Cars"','2023-02-02 00:00:00'),
+	(100005,'"Asgard"','2023-02-02 00:00:00'),
+	(100006,'"Retro Tech"','2023-02-05 00:00:00'),
+	(100004,'"Science"','2023-02-05 00:00:00');
+
+INSERT INTO db.Friends VALUES 
+	(100001,100003,'2023-01-30 00:00:00'),
+	(100001,100009,'2023-01-31 00:00:00'),
+	(100003,100001,'2023-01-31 00:00:00'),
+	(100005,100003,'2023-02-01 00:00:00'),
+	(100007,100003,'2023-02-01 00:00:00'),
+	(100007,100005,'2023-02-03 00:00:00'),
+	(100008,100007,'2023-02-07 00:00:00'),
+	(100009,100002,'2023-02-09 00:00:00'),
+	(100002,100009,'2023-02-09 00:00:00');
+
+INSERT INTO db.Photo (albumID, caption, data) VALUES 
+	(200000,'This is a caption','/path/to/photo1.jpg'),
+	(200002,'This is another caption','/path/to/photo2.jpg'),
+	(200003,'We''ve got so many captions','/path/to/photo3.jpg'),
+	(200002,'Yet another caption','/path/to/photo4.jpg'),
+	(200004,'Something about captions','/path/to/photo5.jpg'),
+	(200005,'Captions?','/path/to/photo6.jpg'),
+	(200004,'Captions!','/path/to/photo7.jpg'),
+	(200005,'Wowie, another caption!','/path/to/photo8.jpg'),
+	(200006,'Will the captions ever stop?','/path/to/photo9.jpg'),
+	(200007,'Yep. This is the last caption.','/path/to/photo10.jpg');
 
 INSERT INTO db.Tag VALUES ('"friends"',300001),
-	('"food"',300001),
+	('"food"',300000),
 	('"fun"',300002),
 	('"food"',300003),
 	('"longweekend"',300003),
@@ -125,15 +137,16 @@ INSERT INTO db.Tag VALUES ('"friends"',300001),
 	('"food"',300009),
 	('"drinks"',300009),
 	('"fun"',300009),
-	('"friends"',300010);
+	('"friends"',300007);
 
-INSERT INTO db.Comment VALUES (400001,'This is a comment','2023-01-31 00:00:00',100002,300003),
-	(400002,'This is another comment','2023-02-01 00:00:00',100010,300001),
-	(400003,'Yet another comment','2023-02-01 00:00:00',100005,300004),
-	(400004,'More comments? You bet!','2023-02-02 00:00:00',100003,300002),
-	(400005,'We''ve got lots of comments','2023-02-03 00:00:00',100007,300008),
-	(400006,'Comments galore!','2023-02-03 00:00:00',100007,300007),
-	(400007,'The comments wont ever stop!','2023-02-03 00:00:00',100007,300005),
-	(400008,'Nah, there''s only 3 more','2023-02-04 00:00:00',100006,300010),
-	(400009,'Well heres another','2023-02-05 00:00:00',100004,300009),
-	(400010,'Finally! The last comment!','2023-02-05 00:00:00',100001,300006);
+INSERT INTO db.Comment (content, dateCreated, userID, photoID) VALUES 
+	('This is a comment','2023-01-31 00:00:00',100002,300003),
+	('This is another comment','2023-02-01 00:00:00',100000,300001),
+	('Yet another comment','2023-02-01 00:00:00',100005,300004),
+	('More comments? You bet!','2023-02-02 00:00:00',100003,300002),
+	('We''ve got lots of comments','2023-02-03 00:00:00',100007,300008),
+	('Comments galore!','2023-02-03 00:00:00',100007,300007),
+	('The comments wont ever stop!','2023-02-03 00:00:00',100007,300005),
+	('Nah, there''s only 3 more','2023-02-04 00:00:00',100006,300000),
+	('Well heres another','2023-02-05 00:00:00',100004,300009),
+	('Finally! The last comment!','2023-02-05 00:00:00',100001,300006);
