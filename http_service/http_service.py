10a63712a1a0b1e40b0fb16b7fb98ceda7bb4cab
Mitchell Frazer <108438939+moosieth@users.noreply.github.com>
2023-04-07 11:24:28 -0700

    Continue Flask API implementation (#2)
    * docs: add friendrec path to api spec
    
    Add docs on /friendrec API endpoint
    
    * feat: switch to master add/rm, implement photos by tag and trending tags
    
    Switch to Master ADD/RM endpoints with "target" param. Implement all photos by tags and trending tags.
    
    BREAKING CHANGE!: Trending Tags not returning expected number of results
    
    * feat: implement comment search, photo rec, and recent photos in api
    
    Implement methods to search comments, reccomend photos, and show photos by recency into flask API
    
    BREAKING CHANGE: Docs not yet implemented, some bugs exist (see TODOs in code)
    
    * feat/docs: finish api docs, move remove to removebyid
    
    Finish API spec docs. Change remove method to removal for attributes with IDS. other removals to be added later.
    
    * chore: change obe function name
    
    * docs(chore): add left-out docs for trending tags method
    
    * docs: formatting changes

--- http_service/http_service.py
+++ http_service/http_service.py
@@ -123,31 +123,11 @@ def contrib():
 
     return jsonify(tuples)
 
-@app.route('/addalbum', methods=['POST'])
-def addalbum():
-    newAlbum = request.json
-
-    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
-    cursor = con.cursor()
-
-    query = f'INSERT INTO Album (userID, name, dateCreated) VALUES({newAlbum["userID"]}, "{newAlbum["name"]}", "{newAlbum["dateCreated"]}")'
-
-    try:
-        cursor.execute(query)
-        con.commit()
-        cursor.close()
-        con.close()
-
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-    except mysql.connector.Error as e:
-        print("MYSQL EXECUTION ERROR: {}".format(e))
-        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
-
 # Search for a user's album. Used in ensuring Album exists when posting photo, or when looking through users albums.
 @app.route('/searchalbum', methods=['GET'])
 def searchalbum():
-    uid = request.args.get('uid')
-    aName = request.args.get("albumName")
+    uid = request.args.get("uid")
+    aName = request.args.get("name")
 
     con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
     cursor = con.cursor()
@@ -157,14 +137,26 @@ def searchalbum():
 
     return jsonify(tuples)
 
-@app.route('/addphoto', methods=['POST'])
-def addphoto():
-    newPhoto = request.json
+@app.route('/add', methods=['POST'])
+def add():
+    newEntry = request.json
 
     con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
     cursor = con.cursor()
     
-    query = f'INSERT INTO Photo (albumID, caption, data) VALUES({newPhoto["albumID"]}, "{newPhoto["caption"]}", "{newPhoto["data"]}")'
+    if newEntry["target"] == 'photo':
+         query = f'INSERT INTO Photo (albumID, caption, data) VALUES({newEntry["albumID"]}, "{newEntry["caption"]}", "{newEntry["data"]}")'
+    elif newEntry["target"] == 'album':
+        query = f'INSERT INTO Album (userID, name, dateCreated) VALUES({newEntry["userID"]}, "{newEntry["name"]}", "{newEntry["dateCreated"]}")'
+    elif newEntry["target"] == 'friend':
+        query = f'INSERT INTO Friends VALUES({newEntry["userID"]}, "{newEntry["friendID"]}", "{newEntry["dateFormed"]}")'
+    elif newEntry["target"] == 'likes':
+        query = f'INSERT INTO Likes VALUES({newEntry["userID"]}, "{newEntry["photoID"]}")'
+    elif newEntry["target"] == 'comment':
+        query = f'INSERT INTO Comment (content, dateCreated, userID, photoID) VALUES({newEntry["content"]}, {newEntry["dateCreated"]}, {newEntry["userID"]}, "{newEntry["photoID"]}")'
+    else:
+        print("SKILL ISSUE: User provided JSON not of correct format")
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
 
     try:
         cursor.execute(query)
@@ -176,8 +168,127 @@ def addphoto():
     except mysql.connector.Error as e:
         print("MYSQL EXECUTION ERROR: {}".format(e))
         return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+    
+@app.route('/removebyid', methods=['POST'])
+def removebyid():
+    data = request.json
 
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
 
+    if data["target"] == 'user':
+        query = f'DELETE FROM User WHERE userID={data["id"]}'
+    elif data["target"] == 'photo':
+        query = f'DELETE FROM Photo WHERE photoID={data["id"]}'
+    elif data["target"] == 'album':
+        query = f'DELETE FROM Album WHERE albumID={data["id"]}'
+    elif data["target"] == 'comment':
+        query = f'DELETE FROM Comment WHERE albumID={data["id"]}'
+    else:
+        print("SKILL ISSUE: User provided JSON not of correct format")
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+    
+    try:
+        cursor.execute(query)
+        con.commit()
+        cursor.close()
+        con.close()
+
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    except mysql.connector.Error as e:
+        print("MYSQL EXECUTION ERROR: {}".format(e))
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+
+@app.route('/photobytag', methods=['GET'])
+def photobytag():
+    tagName = request.args.get("name")
+
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    query = f'SELECT Photo.photoID, Photo.caption, Photo.data FROM Photo INNER JOIN Tag ON Photo.photoID = Tag.photoID WHERE Tag.name = "{tagName}"'
+
+    cursor.execute(query)
+    tuples = cursor.fetchall()
+
+    if(tuples):
+        return jsonify(tuples)
+    else:
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+    
+@app.route('/trendingtags', methods=['GET'])
+def trendingtags():
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    # TODO: Not getting 10 results, need 2 fix
+    cursor.execute('SELECT name, COUNT(name) FROM Tag GROUP BY name ORDER BY COUNT(name) DESC LIMIT 10')
+    tuples = cursor.fetchall()
+
+    if(tuples):
+        return jsonify(tuples)
+    else:
+        return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
+
+@app.route('/searchcom', methods=['GET'])
+def searchcom():
+    content = request.args.get("content")
+
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    query1 = f'CREATE VIEW CommentSearch AS (SELECT userID FROM Comment WHERE content LIKE "%{content}%")'
+    query2 = f'SELECTUser.fName, User.lName, COUNT(CommentSearch.userID) FROM User INNER JOIN CommentSearch ON User.userID = CommentSearch.userID GROUP BY fName, lName ORDER BY COUNT(CommentSearch.userID) DESC'
+
+    cursor.execute(query1)
+    cursor.execute(query2)
+    tuples = cursor.fetchall()
+
+    cursor.execute(f'DROP VIEW CommentSearch')
+
+    if(tuples):
+        return jsonify(tuples)
+    else:
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+
+@app.route('/photorec', methods=['GET'])
+def photorec():
+    uid = request.args.get("uid")
+
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    query1 = f'CREATE VIEW {uid}Photos AS (SELECT Photo.photoID AS pid FROM Photo INNER JOIN Album ON Photo.albumID = Album.albumID WHERE Album.userID = {uid})'
+    query2 = f'CREATE VIEW {uid}Tags AS (SELECT Tag.name FROM Tag INNER JOIN Photo ON Tag.photoID = Photo.photoID WHERE Photo.photoID IN (SELECT pid FROM {uid}Photos) GROUP BY Tag.name ORDER BY COUNT(*) DESC LIMIT 5)'
+    query3 = f'SELECT * FROM Photo WHERE photoID IN (SELECT Tag.photoID FROM Tag INNER JOIN {uid}Tags ON Tag.name = {uid}Tags.name) AND photoID NOT IN (SELECT pid FROM {uid}Photos) GROUP BY photoID ORDER BY COUNT(*) DESC LIMIT 50'
+
+    cursor.execute(query1)
+    cursor.execute(query2)
+    cursor.execute(query3)
+    tuples = cursor.fetchall()
+
+    cursor.execute(f'DROP VIEW {uid}Tags')
+    cursor.execute(f'DROP VIEW {uid}Photos')
+
+    if(tuples):
+        return jsonify(tuples)
+    else:
+        return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+
+@app.route('/recentphotos', methods=['GET'])
+def recentphotos():
+    con = mysql.connector.connect(user='root', password='password', host='localhost', database='db')
+    cursor = con.cursor()
+
+    query = f'SELECT * FROM Photo ORDER BY photoID DESC LIMIT 50'
+
+    cursor.execute(query)
+    tuples = cursor.fetchall()
+
+    if(tuples):
+        return jsonify(tuples)
+    else:
+        return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
     app.run()
