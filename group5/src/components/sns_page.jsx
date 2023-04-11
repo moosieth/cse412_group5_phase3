8434f02e8aa5bd4e4ab3f4300a7e18c91b76eb80
Woojeh <93624334+mastee7@users.noreply.github.com>
2023-04-11 02:36:32 -0700

    Add files via upload

---
+++ group5/src/components/sns_page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SNS() {
+  return (
+    <div>
+      <h1>Welcome to the SNS Feeling Page</h1>
+      <p>Share how you're feeling with your friends!</p>
+      <form>
+        <label htmlFor="feeling">Feeling:</label>
+        <input type="text" id="feeling" name="feeling" />
+        <br />
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message"></textarea>
+        <br />
+        <button type="submit">Share</button>
+      </form>
+    </div>
+  );
+}
