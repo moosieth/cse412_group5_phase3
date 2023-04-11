31dbe86549016289696e9210dff4cd96c852856f
Woojeh <93624334+mastee7@users.noreply.github.com>
2023-04-11 02:35:20 -0700

    changes page when login success

--- group5/src/App.jsx
+++ group5/src/App.jsx
@@ -1,9 +1,15 @@
-import Welcome from "./components/login_page"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./components/login_page";
+import SNS from "./components/sns_page";
 
 export default function App() {
-return (
-<>
-<Welcome />
-</>
-);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/social-network-service" element={<SNS />} />
+      </Routes>
+    </Router>
+  );
 }
