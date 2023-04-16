9ebae2d2384fbb5bdc91734f14aa546b0467a1dd
woojeh <woojeh7@gmail.com>
2023-04-16 15:05:22 -0700

    try docker-compose

--- group5/src/layouts/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} INQL Social Network Service</p>
-    </footer>
-  );
-}
